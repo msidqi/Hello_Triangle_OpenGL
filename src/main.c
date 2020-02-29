1fc29e90fa7e90bf950978570a8baae185c63dda
msidqi <mouadmr89@gmail.com>
2020-03-01 00:38:24 +0100

    launch glfw loop, start vertex shader

--- src/main.c
+++ src/main.c
@@ -7,26 +7,67 @@ typedef struct	s_wind
 	GLFWwindow* window;
 }				t_wind;
 
-int main(void)
+void	framebuffer_size_callback(GLFWwindow* window, int width, int height) // callback to resize the viewport @ window size-change
+{
+    glViewport(0, 0, width, height);
+}
+
+void	processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int		init_setup(GLFWwindow **window, int width, int height, char *window_name)
 {
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // load only core OpenGL
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for Mac OS to Initialize
-  
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
+	(*window) = glfwCreateWindow(width, height, window_name, NULL, NULL);
+	if ((*window) == NULL)
 	{
 		printf("Failed to create GLFW window");
+		return (0);
+	}
+	glfwMakeContextCurrent(*window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // load OpenGL functions with GLAD
+	{
+		printf("Failed to initialize GLAD");
+		return (0);
+	}
+	glfwSetFramebufferSizeCallback(*window, framebuffer_size_callback);
+	return (1);
+}
+
+int		main(void)
+{
+	GLFWwindow* window;
+
+	window = NULL;
+	if (!init_setup(&window, 800, 600, "OpenGL"))
+	{
 		glfwTerminate();
 		return (-1);
 	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+	};
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	while(!glfwWindowShouldClose(window))
 	{
-		printf("Failed to initialize GLAD");
-		return -1;
-	}    
+		// glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		processInput(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return (0);
 }
